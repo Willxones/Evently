b4b3054eb964a327e490e079ab0ba35c1e3acb38
Willxones <willtomjones01@gmail.com>
2025-02-11 21:40:25 +0000

    EA-18 Supabase and prisma set up for local dev with example

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,34 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import type { Database } from "../src/database.types.ts"; // Adjust this based on where your file is
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+type Example = Database["public"]["Tables"]["Example"]["Row"];
+
+async function main() {
+	const postedExample = await prisma.example.create({
+		data: {
+			title: "Test",
+		},
+	});
+	//change to reference a table in your schema
+	const val: Example[] = await prisma.example.findMany({
+		take: 10,
+	});
+	console.log(val);
+}
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
 app.use(express.json());
 
 app.get("/", (req, res) => {
