dbc477fdd399be34967aa6a50c5e9a614e7012c4
Willxones <willtomjones01@gmail.com>
2025-03-30 22:29:07 +0100

    EA-27 Auth middleware added

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import { supabase } from "../utils/supabase.js";
+
+export async function authenticateToken(
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> {
+	const authHeader = req.headers["authorization"];
+	const jwt = authHeader && authHeader.split(" ")[1];
+
+	if (!jwt) {
+		res.sendStatus(401);
+		return;
+	}
+
+	try {
+		const { data, error } = await supabase.auth.getUser(jwt);
+
+		if (error || !data.user) {
+			res.sendStatus(401);
+			return;
+		}
+
+		next();
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+		return;
+	}
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,17 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import type { Database } from "../src/database.types.ts";
+import { authenticateToken } from "./middlewares/auth.js";
 
-const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-type Example = Database["public"]["Tables"]["Example"]["Row"];
-
-async function main() {
-	const postedExample = await prisma.example.create({
-		data: {
-			title: "Test",
-		},
-	});
-	const val: Example[] = await prisma.example.findMany({
-		take: 10,
-	});
-	console.log(val);
-}
-main()
-	.then(async () => {
-		await prisma.$disconnect();
-	})
-	.catch(async (e) => {
-		console.error(e);
-		await prisma.$disconnect();
-		process.exit(1);
-	});
 app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Welcome to Evently!");
+});
+app.get("/authed", authenticateToken, (req, res) => {
+	res.send("Welcome to Authed Evently!");
 });
 
 app.listen(PORT, () => {
