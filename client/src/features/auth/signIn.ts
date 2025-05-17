7a96e9dd46f14951e472c030692d1f852ad91cdc
Will <73492215+Willxones@users.noreply.github.com>
2025-05-17 12:36:38 +0100

    EA-63 Frontend added with basic auth
    * EA-63 React client application added
    
    * EA-63 Create frontend application with auth

---
+++ client/src/features/auth/signIn.ts
@@ -0,0 +1,10 @@
+import { supabase } from "../../lib/supabase";
+
+export const signIn = async (email: string, password: string) => {
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
+	if (error) throw new Error(error.message);
+	return data;
+};
