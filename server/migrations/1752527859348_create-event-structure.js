8131929e1ee9a32ff76c37f1cc1e1b32582e677d
Andrew <andrw.smth@outlook.com>
2025-07-20 13:43:33 +0100

    update id and remove is cancelled

--- server/migrations/1752527859348_create-event-structure.js
+++ server/migrations/1752527859348_create-event-structure.js
@@ -8,37 +8,61 @@ export const shorthands = undefined;
  */
 export const up = (pgm) => {
   pgm.createTable('event', {
-    id: { type: 'varchar(36)', primaryKey: true },
-    title: { type: 'varchar(255)', notNull: true },
-    description: { type: 'varchar(1000)', notNull: true },
-    date: { type: 'timestamptz', notNull: true },
-    location: { type: 'varchar(255)', notNull: true },
-    is_cancelled: { type: 'boolean', notNull: true, default: false },
-    organiser_Id: { type: 'varchar(36)', notNull: true, references: '"organiser_profile"(id)', onDelete: 'CASCADE' },
-    created_At: { type: 'timestamptz', notNull: true, default: pgm.func('now()') },
+      id: { type: 'varchar(36)', primaryKey: true },
+      title: { type: 'varchar(255)', notNull: true },
+      description: { type: 'varchar(1000)', notNull: true },
+      date: { type: 'timestamptz', notNull: true },
+      location: { type: 'varchar(255)', notNull: true },
+      organiser_id: {
+          type: 'varchar(36)',
+          notNull: true,
+          references: '"organiser_profile"(id)',
+          onDelete: 'CASCADE',
+      },
+      created_At: { type: 'timestamptz', notNull: true, default: pgm.func('now()') },
   });
 
   pgm.createTable('ticket_type', {
-    id: { type: 'varchar(36)', primaryKey: true },
-    event_Id: { type: 'varchar(36)', notNull: true, references: '"event"(id)', onDelete: 'CASCADE' },
-    name: { type: 'varchar(100)', notNull: true },
-    price: { type: 'integer', notNull: true },
-    quantity: { type: 'integer', notNull: true },
+      id: { type: 'varchar(36)', primaryKey: true },
+      event_id: {
+          type: 'varchar(36)',
+          notNull: true,
+          references: '"event"(id)',
+          onDelete: 'CASCADE',
+      },
+      name: { type: 'varchar(100)', notNull: true },
+      price: { type: 'integer', notNull: true },
+      quantity: { type: 'integer', notNull: true },
   });
 
   pgm.createTable('purchase', {
-    id: { type: 'varchar(36)', primaryKey: true },
-    ticket_type_Id: { type: 'varchar(36)', notNull: true, references: 'Ticket_Type(id)', onDelete: 'CASCADE' },
-    total_Amount: { type: 'integer', notNull: true },
-    created_At: { type: 'timestamptz', notNull: true, default: pgm.func('now()') },
+      id: { type: 'varchar(36)', primaryKey: true },
+      ticket_type_id: {
+          type: 'varchar(36)',
+          notNull: true,
+          references: 'Ticket_Type(id)',
+          onDelete: 'CASCADE',
+      },
+      total_Amount: { type: 'integer', notNull: true },
+      created_At: { type: 'timestamptz', notNull: true, default: pgm.func('now()') },
   });
 
   pgm.createTable('purchase_item', {
-    id: { type: 'varchar(36)', primaryKey: true },
-    purchase_Id: { type: 'varchar(36)', notNull: true, references: 'purchase(id)', onDelete: 'CASCADE' },
-    ticket_Type_Id: { type: 'varchar(36)', notNull: true, references: 'Ticket_Type(id)', onDelete: 'RESTRICT' },
-    quantity: { type: 'integer', notNull: true },
-    amount: { type: 'integer', notNull: true },
+      id: { type: 'varchar(36)', primaryKey: true },
+      purchase_id: {
+          type: 'varchar(36)',
+          notNull: true,
+          references: 'purchase(id)',
+          onDelete: 'CASCADE',
+      },
+      ticket_Type_id: {
+          type: 'varchar(36)',
+          notNull: true,
+          references: 'Ticket_Type(id)',
+          onDelete: 'RESTRICT',
+      },
+      quantity: { type: 'integer', notNull: true },
+      amount: { type: 'integer', notNull: true },
   });
 };
 
