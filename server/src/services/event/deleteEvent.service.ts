6d2a29fe71165df82274b3377ad39fc6be5bd0f9
Andrew <andrw.smth@outlook.com>
2025-08-05 18:15:29 +0100

    seperate out event services

---
+++ server/src/services/event/deleteEvent.service.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { pool } from '../../utils/connections/pg.js';
+
+async function deleteEvent(req: Request, res: Response) {
+    try {
+        const { eventId } = req.params;
+        const result = await pool.query('DELETE FROM public.event WHERE id = $1 RETURNING *', [
+            eventId,
+        ]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Event not found' });
+        }
+
+        return res
+            .status(200)
+            .json({ message: 'Event deleted successfully', event: result.rows[0] });
+    } catch (error) {
+        console.error('Error deleting event:', error);
+        return res.status(500).json({ error: 'Failed to delete event' });
+    }
+}
+export { deleteEvent };
