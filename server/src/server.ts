20772a51db3921173b2ff4526718dd928f88a0fb
Willxones <willtomjones01@gmail.com>
2025-09-02 23:02:18 +0100

    EA-73 Reverted redundant server file changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,10 @@ import organiser from './controllers/organiser/organiserProfile.controller.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/organiser', organiser);
+
 app.use(express.json());
+
 app.use(
     cors({
         origin: 'http://localhost:5173',
@@ -15,12 +18,10 @@ app.use(
     })
 );
 
-app.use('/organiser', organiser);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, World! Welcome to the Evently API!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 });
