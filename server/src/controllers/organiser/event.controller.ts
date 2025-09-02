3864c4a19853b02eab5c2aed2e834b098e970226
Will <73492215+Willxones@users.noreply.github.com>
2025-09-02 23:42:49 +0100

    EA-73 Event CRUD
    * /create-event end point working
    
    * updated
    
    * getEvents endpoint working
    
    * get events by organiser id working
    
    * Update and delete end points working
    
    * tidying up - seperate out into event.controller
    
    * EA-73 Reverted redundant server file changes
    
    * EA-73 Reverted organiser profile controller
    
    * EA-73 Reverted organiser profile controller again
    
    * EA-73 removed event routes from organiser controller
    
    * EA-73 Adjustments to events service
    
    ---------
    
    Co-authored-by: Andrew <andrw.smth@outlook.com>

---
+++ server/src/controllers/organiser/event.controller.ts
@@ -0,0 +1,42 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+
+import { authenticateToken } from '../../middlewares/shared/auth.js';
+import {
+    createEvent,
+    getAllEvents,
+    getEventsByOrganiserId,
+    deleteEvent,
+    updateEvent,
+} from '../../services/organiser/event.service.js';
+
+const router = express.Router();
+
+router.use(express.json());
+router.use(
+    cors({
+        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'authorization'],
+    })
+);
+
+router.post('/create-event', authenticateToken, async (req: Request, res: Response) => {
+    await createEvent(req, res);
+});
+
+router.get('/get-events', authenticateToken, async (req: Request, res: Response) => {
+    await getAllEvents(req, res);
+});
+
+router.get('/get-events/:organiserId', authenticateToken, async (req: Request, res: Response) => {
+    await getEventsByOrganiserId(req, res);
+});
+router.delete('/delete-event/:eventId', authenticateToken, async (req: Request, res: Response) => {
+    await deleteEvent(req, res);
+});
+router.put('/update-event/:eventId', authenticateToken, async (req: Request, res: Response) => {
+    await updateEvent(req, res);
+});
+
+export default router;
